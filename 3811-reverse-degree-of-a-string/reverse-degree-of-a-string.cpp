168c6cf6567135b460dc87adf7ff4bcc6b9d1a21
Prabal Gupta <152983979+1prabal@users.noreply.github.com>
2026-08-10 12:25:14 +0530

    Time: 1 ms (26.38%) | Memory: 11.8 MB (5.29%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        map<char,int> m;
+        int value = 26;
+        for(char i='a'; i<='z'; i++){
+            m[i] = value--;
+        }
+        int ans=0;
+        for(int i=0; i<s.size(); i++){
+            ans += m[s[i]]*(i+1);
+        }
+        return ans;
+    }
+};
