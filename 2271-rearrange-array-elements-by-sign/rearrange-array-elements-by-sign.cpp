ca24ee671f599477513fd602ff521a1db24b29d9
Prabal Gupta <152983979+1prabal@users.noreply.github.com>
2026-08-18 23:20:19 +0530

    Time: 16 ms (8.32%) | Memory: 145 MB (10.00%) - LeetSync

---
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int> pos;
+        vector<int> neg;
+        vector<int> v;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]>=0){
+                pos.push_back(nums[i]);
+            }else{
+                neg.push_back(nums[i]);
+            }
+        }
+        for(int i=0; i<pos.size(); i++){
+            v.push_back(pos[i]);
+            v.push_back(neg[i]);
+        }
+        return v;
+    }
+};
