2e2630238ceee35be213c31ec7be1d074665de1e
Prabal Gupta <152983979+1prabal@users.noreply.github.com>
2026-08-21 10:25:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (84.55%) - LeetSync

---
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+            int count=0, ans=0;
+        for(int i=0; i<s.size(); i++){
+            if(s[i]=='L') count++;
+            if(s[i]=='R') count--;
+            if(count==0) ans++;
+        }
+        return ans;
+    }
+};
